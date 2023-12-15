4ad02c9dee4993fbacfa940c9f3bd8d10bd4af21
mark-bernstorf <141446271+mark-bernstorf@users.noreply.github.com>
2023-12-16 03:35:00 +0600

    Add files via upload

--- part1.py
+++ part1.py
@@ -1,100 +1,111 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
-foptions = webdriver.FirefoxOptions()
-foptions.accept_insecure_certs = True
-foptions.set_preference('security.cert_pinning.enforcement_level', 0)
-foptions.set_preference('security.enterprise_roots.enabled', 1)
-foptions.set_preference('general.useragent.override', 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0')
-foptions.set_preference('dom.webdriver.enabled', False)
 
-#проксю используем как в настройках системы
-foptions.set_preference('network.proxy.type', 5)
-
-browser = webdriver.Firefox(options=foptions)
-button, block, element = {}, {}, {}
-
-browser.get('https://www.nseindia.com')
-
-# напишем процедуру, которая будет выдавать элементы по мере их появления (ждать, когда загрузятся таблицы, графики и т.д.)
-def wait_element(parent, by_, str_, attempt = 0, sleep = 1, single = True, hasbool = False):
+def wait_element(parent, by_, str_, attempt=0, sleep=1.0, single=True, hasbool=False):
     '''
-    attempt = число попыток
-    sleep = время ожидания после последней неудачи
-    single = ищем один элемент (True) или список элементов (False)
-    hasbool = выводит буловскую переменую вместо объекта, True = успех, False = провал
+    Процедура ожидает появления элемента с заданым колличеством попыток и временем ожидания.
+
+    Аргументы:
+    parent - объект, в котором происходит поиск;
+    by_ - по какому локатору осуществляется поиск;
+    str_ - искомая строка;
+    attempt - число попыток, если == 0 то неограничено;
+    sleep - время ожидания после последней неудачи;
+    single - ищем один элемент (True) или список элементов (False);
+    hasbool - выводит буловскую переменую вместо объекта;
+
+    Пример использования:
+    div_element = wait_element(browser, By.CLASS_NAME, 'div');
     '''
     element_not_rdy = True
-    try_count = 0
+    try_count = 1  # Задаем счётчик попыток
     while element_not_rdy:
+        # Если не привышен лимит попыток, либо если не задан лимит пробуем получить элементы
         if (attempt <= try_count):
             try:
-                if single:
-                    element = parent.find_element(by_, str_)
-                else:
-                    element = parent.find_elements(by_, str_)
+                # При single == True получаем один элемент, при False = список элементов
+                element = parent.find_element(
+                    by_, str_) if single else parent.find_elements(by_, str_)
                 element_not_rdy = False
-            except:
+            except NoSuchElementException:
+                # Если ошибка связана с тем, что элемент не найден, ожидаем некоторое время перед началом новой попытки
                 time.sleep(sleep)
             if attempt > 0:
                 try_count += 1
         else:
+            # В случае исчерпания попыток выводим сообщение, что элемент найти не удалось
             print('element not exist:', by_, str_)
-            if hasbool:
+            if hasbool:  # Если нужен булевский результат выводим его
                 return False
-                exit()
     if hasbool:
         return True
     return element
 
-#часть 1
+
+# Перед созданием экземпляра WebDriver.Firefox зададим первоначальные настройки:
+foptions = webdriver.FirefoxOptions()
+# Будем доверять небезопасным сертификатам
+foptions.accept_insecure_certs = True
+# Пининг сертификатов не используется
+foptions.set_preference('security.cert_pinning.enforcement_level', 0)
+# Доверять корневым сертификатам, установленным и управляемым организацией или предприятием
+foptions.set_preference('security.enterprise_roots.enabled', 1)
+# Выдаем себя за обычный браузер
+foptions.set_preference('general.useragent.override',
+                        'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0')
+# Скрываем использование автоматизированных средств.
+foptions.set_preference('dom.webdriver.enabled', False)
+# Прокси используем как в настройках системы
+foptions.set_preference('network.proxy.type', 5)
+# Создаем экземпляр браузера Mozilla Firefox с определенными выше параметрами веб-драйвера
+browser = webdriver.Firefox(options=foptions)
+# Для удобства и более понятного визуального понимания кода создадим словари с элементами
+button, block, element = {}, {}, {}
+# Приступаем к выполнению первой части задания
 desc = '''
     1. Зайти на https://www.nseindia.com
     2. Навестись (hover) на MARKET DATA
     3. Кликнуть на Pre-Open Market
     4. Спарсить данные Final Price по всем позициям на странице и вывести их в csv файл. Имя;цена
+    upd: по рекомендации используем CSS_SELECTOR там, где это более оправдано
 '''
-
 print(desc)
-
-
-button['MARKET DATA'] = browser.find_element(By.XPATH,'//*[@id="link_2"]')
+browser.get('https://www.nseindia.com')  # Зайти на https://www.nseindia.com
+button['MARKET DATA'] = browser.find_element(
+    By.CSS_SELECTOR, '#link_2')  # Поиск и выбор "MARKET DATA"
 button['MARKET DATA'].click()
-
-#button['Pre-Open Market'] = browser.find_element(By.LINK_TEXT,'Pre-Open Market')
-button['Pre-Open Market'] = browser.find_element(By.XPATH,'/html/body/header/nav/div[2]/div/div/ul/li[3]/div/div[1]/div/div[1]/ul/li[1]/a')
-#ошибка сертификата внезапно лечится убиением всех печенек
+# Ищем и кликаем на Pre-Open Market
+button['Pre-Open Market'] = browser.find_element(
+    By.PARTIAL_LINK_TEXT, 'Pre-Open Market')
+# Ошибка сертификата внезапно лечится убиением всех печенек
 browser.delete_all_cookies()
 button['Pre-Open Market'].click()
-
-#block['table'] = browser.find_element(By.ID, 'livePreTable');
-block['table'] = wait_element(browser, By.XPATH, 'html/body/div[10]/div/section/div/div/div/div/div/div/div[3]/div/table/tbody');
-if wait_element(block['table'], By.CLASS_NAME,'symbol-word-break', hasbool=True, single=False):
-    element['label'] = block['table'].find_elements(By.CLASS_NAME,'symbol-word-break')
-if wait_element(block['table'], By.CLASS_NAME,'bold', hasbool=True, single=False):
-    element['prise'] = block['table'].find_elements(By.CLASS_NAME,'bold') #они единственные жирные
-
-#print(element['label'], element['prise'])
-labels, prises = [], []
-
+block['table'] = wait_element(
+    browser, By.CSS_SELECTOR, '#livePreTable')  # Ждём таблицу
+# Ждём загрузки таблицы и получаем имена
+if wait_element(block['table'], By.CLASS_NAME, 'symbol-word-break', hasbool=True, single=False):
+    element['label'] = block['table'].find_elements(
+        By.CLASS_NAME, 'symbol-word-break')
+# Ищём в таблице "Prise"
+if wait_element(block['table'], By.CLASS_NAME, 'bold', hasbool=True, single=False):
+    element['prise'] = block['table'].find_elements(
+        By.CLASS_NAME, 'bold')  # Они единственные жирные
+labels, prises = [], []  # Создадим списки с именем и ценой
 for i in element['label']:
     labels.append(i.text)
-
 for i in element['prise']:
     prises.append(i.text)
-
+# Сохраняем результат в CSV файл
 with open('part1.csv', 'w', newline='', encoding='utf8') as file:
     writer = csv.writer(file)
-    writer.writerow(('label', 'price')) 
+    writer.writerow(('label', 'price'))
     for i in zip(labels, prises):
-        writer.writerow(i) 
-
-#ждём пару секунд, переходим ко второй части
-time.sleep(2)
-
-#часть 2
+        writer.writerow(i)
+time.sleep(2)  # Ждём пару секунд, переходим ко второй части
 desc = '''
     1. Зайти на главную страницу
     3. Выбрать график "NIFTY BANK"
@@ -104,29 +115,29 @@ desc = '''
     6. Пролистать таблицу до конца
 '''
 print(desc)
-
-#1 заходим на страницу
 browser.delete_all_cookies()
-browser.get('https://www.nseindia.com')
-#2 ищем кнопку, тыкаем
-button['NIFTY BANK'] = browser.find_element(By.XPATH, '//*[@id="tabList_NIFTYBANK"]')
+browser.get('https://www.nseindia.com')  # Зайти на главную страницу
+button['NIFTY BANK'] = browser.find_element(
+    By.CSS_SELECTOR, '#tabList_NIFTYBANK')  # Выбрать график "NIFTY BANK"
 button['NIFTY BANK'].click()
-#3 скролим до графика
-block['plot'] = browser.find_element(By.XPATH, '//*[@id="tab4_container"]')
+block['plot'] = browser.find_element(
+    By.CSS_SELECTOR, '#tab4_container')  # Ждём график
+# Пролистаем вниз до графика
 browser.execute_script("arguments[0].scrollIntoView(true);", block['plot'])
-#4 тыкаем на view all
 browser.delete_all_cookies()
-button['View all'] = wait_element(browser, By.CSS_SELECTOR, '#tab4_gainers_loosers > div:nth-child(3) > a:nth-child(1)')
+# Ищем и тыкаем на view all
+button['View all'] = wait_element(
+    browser, By.CSS_SELECTOR, '#tab4_gainers_loosers > div:nth-child(3) > a:nth-child(1)')  
 button['View all'].click()
-
-# тыкаем на "NIFTY ALPHA 50"
-button['selector'] = wait_element(browser, By.XPATH, '/html/body/div[10]/div/section/div/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/select/optgroup[4]/option[7]')
+# Ищем и тыкаем на "NIFTY ALPHA 50"
+button['selector'] = wait_element(
+    browser, By.CSS_SELECTOR, '#equitieStockSelect > optgroup:nth-child(4) > option:nth-child(7)')
 button['selector'].click()
-
-#ну и сразу перематываем к концу таблицы, начало другого блока
-block['note'] = wait_element(browser, By.CLASS_NAME, '/html/body/div[10]/div/section/div/div/div/div/div[1]/div/div/div/div[5]')
+# Поиск блока в конце таблицы
+block['note'] = wait_element(
+    browser, By.CSS_SELECTOR, 'div.note_container:nth-child(5)')
+# Пролистываем до него
 browser.execute_script("arguments[0].scrollIntoView(true);", block['note'])
-
-#ждём 5 секунд, закрываем браузер
-time.sleep(5)
+print('Задание выполнено')
+time.sleep(5)  # Ждём 5 секунд перед закрытием браузера
 browser.quit()
--- part2.py
+++ part2.py
@@ -4,15 +4,18 @@ import json
 import re
 from bs4 import BeautifulSoup as bsoup
 
+# Перед отправкой запроса сформируем минимальный заголовок, представимся реальным браузером
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
     'Accept': '*/*',
 }
-
+# Нужно перейти по ссылке https://2ip.ru/ и спарсить свой ip-адрес
 response = requests.get('https://2ip.ru')
+# Для удобства будем использовать BeautifulSoup при парсинге
 ip = bsoup(response.text, 'html.parser').find('div', class_='ip').text.strip()
-
-#путь самурая
+print('Получили IP:', ip)
+# Нужно перейти по ссылке https://www.maxmind.com/en/geoip2-precision-demo и получить название таймзоны у своего ip-адреса
+# Сформируем заголовок таким, как если бы мы заходили с реального браузера и перейдём по ссылке
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
     'Accept': '*/*',
@@ -24,73 +27,51 @@ headers = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
 }
-
-response = requests.get('https://www.maxmind.com/en/geoip2-precision-demo', headers)
-
-pos_start   = response.text.find('window.MaxMind.X_CSRF_TOKEN = "') + 31
-pos_end     = response.text.rfind('";')
-token = response.text[pos_start:pos_end]
-cookies = {'mm_session':response.headers['set-cookie'].split()[0][11:-1]}
-
-#print(token, cookies)
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Referer': 'https://www.maxmind.com/en/geoip2-precision-demo',
-    'X-CSRF-Token': token,
-    'X-Requested-With': 'XMLHttpRequest',
-    'Origin': 'https://www.maxmind.com',
-    'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-}
-
-response = requests.post('https://www.maxmind.com/en/geoip2/demo/token', cookies=cookies, headers=headers, allow_redirects=True, verify=False)
-#print(response.text)
-
+response = requests.get(
+    'https://www.maxmind.com/en/geoip2-precision-demo', headers)
+# Нам необходимо достать X-CSRF-Token и запомнить куки, которые пришли в ответ на запрос
+# 1. Получаем X-CSRF-Token:
+pos_start = response.text.find('window.MaxMind.X_CSRF_TOKEN = "') + 31
+pos_end = response.text.rfind('";')
+x_csrf_token = response.text[pos_start:pos_end]
+# 2. Запоминаем куки
+cookies = {'mm_session': response.headers['set-cookie'].split()[0][11:-1]}
+print(f'X-CSRF-Token = {x_csrf_token}, cookies = {cookies}')
+# Передаем полученый токен в заголовок
+headers['X-CSRF-Token'] = x_csrf_token
+# Теперь нам нужно получить токен уже для доступа к geoip.maxmind.com
+# Отправляем запрос с токеном в заголовке и печеньками, разрешаем редиректы и отключаем проверку сертификата
+response = requests.post('https://www.maxmind.com/en/geoip2/demo/token',
+                         cookies=cookies, headers=headers, allow_redirects=True, verify=False)
+# Нам пришёл токен для авторизации, сразу представим его в виде json объекта
 auth_token = json.loads(response.text)
-#print(auth_token['token'])
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    # 'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://www.maxmind.com/',
-    'Authorization': 'Bearer '+auth_token['token'],
-    'Origin': 'https://www.maxmind.com',
-    'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    # Requests doesn't support trailers
-    # 'TE': 'trailers',
-}
-
-params = {
-    'demo': '1',
-}
-
-response = requests.get('https://geoip.maxmind.com/geoip/v2.1/city/'+ip, params=params, headers=headers)
+print(auth_token['token'])
+# Теперь мы можем получить информацию о временой зоне нашего адреса
+# Подставляем в заголовок полученый токен для авторизации
+headers['Authorization'] = 'Bearer '+auth_token['token']
+# Передадим парамерт demo=1
+params = {'demo': '1'}
+# Отправляем запрос с токеном авторизации в заголовке и параметром, подставляем IP
+response = requests.get(
+    'https://geoip.maxmind.com/geoip/v2.1/city/'+ip, params=params, headers=headers)
+# В результате получаем много полезной информации в формате JSON, там есть и интересующая нас временая зона
 result = json.loads(response.text)
-#print(result['location']['time_zone'])
+print(f'full json = {response.text}')
+print(f'only time_zone = {result["location"]["time_zone"]}')
 time_zone = result['location']['time_zone']
-
+# Теперь нужно перейти по ссылке https://gist.github.com/salkar/19df1918ee2aed6669e2 и получить список регионов, входящих в полученную таймзону
+# Снова представимся обычным браузером и отправим запрос
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
     'Accept': '*/*',
 }
-
 response = requests.get('https://gist.github.com/salkar/19df1918ee2aed6669e2')
-#print(response.text)
-
-regions = re.findall(r'\[&quot;(.*)&quot;, &quot;'+time_zone+'&quot;\]', response.text)
-#print(regions)
-
+# Парсим из ответа все строки в которых встречаем нашу временую зону
+regions = re.findall(r'\[&quot;(.*)&quot;, &quot;' +
+                     time_zone+'&quot;\]', response.text)
+print(regions)
+# Сохраняем результат в текстовый файл
 with open("part2.txt", "w") as file:
     file.write('Зона: ' + time_zone + "\n")
     file.write('Регионы: '+', '.join(regions))
-
-print('ok')
+print('Задание выполнено')
